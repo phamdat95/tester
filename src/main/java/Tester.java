b3f3d95be4930a934df8f94dbf679fe26aa64b26
phamdat95 <phamdatvmu95@gmail.com>
2019-06-20 09:00:16 +0700

    ddd

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("nhap");
-        int so = in.nextInt();
-        System.out.println(so);
-        ArrayList<Integer> array = new ArrayList<Integer>();
-        long time = System.nanoTime();
-        for (int i = 0; i <= so; i++) {
-            boolean inter = true;
-            for (int j = 2; j < i; j++) {
-                if (i % j == 0) {
-                    inter = false;
-                    break;
-                }
-            }
-            if (inter) {
-                array.add(i);
-            }
-        }
-        long time1 = System.nanoTime();
-        System.out.println(time1 - time);
-        System.out.println(array);
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("nhap");
+//        int so = in.nextInt();
+//        System.out.println(so);
+//        ArrayList<Integer> array = new ArrayList<Integer>();
+//        long time = System.nanoTime();
+//        for (int i = 0; i <= so; i++) {
+//            boolean inter = true;
+//            for (int j = 2; j < i; j++) {
+//                if (i % j == 0) {
+//                    inter = false;
+//                    break;
+//                }
+//            }
+//            if (inter) {
+//                array.add(i);
+//            }
+//        }
+//        long time1 = System.nanoTime();
+//        System.out.println(time1 - time);
+//        System.out.println(array);
     }
 }
